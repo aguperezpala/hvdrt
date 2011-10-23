667b5b05728f52da8ff25916ec600bed0562f8e0
aguperezpala@gmail.com <aguperezpala@gmail.com@06da5462-caec-b45d-aab9-b80d20edd223>
2011-10-23 03:33:21 +0000

    Working on ZoneSelector

--- trunk/src/GUI/ZoneSelector/MouseQLabel.h
+++ trunk/src/GUI/ZoneSelector/MouseQLabel.h
@@ -7,6 +7,7 @@
 #include <qgraphicsitem.h>
 #include <qlabel.h>
 #include <qwidget.h>
+#include <qpainter.h>
 
 
 #include <opencv2/core/core.hpp>
@@ -20,6 +21,8 @@ class MouseQLabel : public QLabel
 {
     Q_OBJECT
 
+    static const int REMARK_POINT_PIXELS	=	4;
+    static const int REMARK_POINT_COLOR		=	255;
 public:
     MouseQLabel(QWidget *parent = 0);
     ~MouseQLabel();
@@ -31,10 +34,23 @@ public:
     inline int getX(void) const {return mX;}
     inline int getY(void) const {return mX;}
 
+    /* Sets the image to work with */
+    void setImage(const QImage &img);
+
+
 protected:
     void mousePressEvent(QMouseEvent* event);
+    void mouseMoveEvent(QMouseEvent* event);
 
 private:
+    /* fills the label with the img */
+    void showImage(const QImage &img);
+
+    /* Remarks the point */
+    void remarkPoint(QImage &img, int x, int y);
+
+    /* Remark a rectangle */
+    void remarkRectangle(QImage &img, int topLeftX, int topLeftY, int bottomRX, int bottomRY);
 
 
 private:
@@ -43,6 +59,7 @@ private:
     QLabel		*mLabelX;
     QLabel		*mLabelY;
 
+    QImage		mImg;
 };
 
 #endif // MOUSEQLABEL_H
