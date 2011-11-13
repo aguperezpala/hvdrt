da243d727c174e5e841ae214f6e43b7ffa1323f4
aguperezpala@gmail.com <aguperezpala@gmail.com@06da5462-caec-b45d-aab9-b80d20edd223>
2011-11-13 18:53:30 +0000

    Adding CallBackFunctor. Working the main WaveHIPS (all the process). Working on the DataProcessor (to save in the file the real height in millimeters and to apply some logic to the data)

---
+++ trunk/src/GUI/IPSModule/ImageProcessingSystem/WaveHeightIPS/ImageAnalizer/BorderDetector/cannyparametercalculator.h
@@ -0,0 +1,65 @@
+#ifndef CANNYPARAMETERCALCULATOR_H
+#define CANNYPARAMETERCALCULATOR_H
+
+#include <QtGui/QWidget>
+#include <qfiledialog.h>
+#include <qdialog.h>
+#include <qmessagebox.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <iostream>
+#include <string>
+
+#include "ui_cannyparametercalculator.h"
+
+//#include "BorderDetector.h"
+
+class CannyParameterCalculator : public QDialog
+{
+    Q_OBJECT
+
+public:
+    CannyParameterCalculator(QWidget *parent = 0);
+    ~CannyParameterCalculator();
+
+    /* Return the parametters */
+    double getThreshold1(void) const {return mThreshold1;}
+    double getThreshold2(void) const {return mThreshold2;}
+    double getL2Gradient(void) const {return mL2Gradient;}
+
+    /* Set an image to analyze */
+    void setImage(const cv::Mat &img);
+
+public slots:
+	void onSlide1Change(int v);
+	void onSlide2Change(int v);
+	void onButtonCannyPressed(void);
+	void onButtonLoadPressed(void);
+	void onButtonShowRealPressed(void);
+	void onButtonDonePressed(void);
+
+private:
+	/* Load the image into the label */
+	bool IplImage2QImage(const cv::Mat &iplImg, QImage &ref);
+	void showImage(const cv::Mat &img);
+
+	/* Error message */
+	int showMessage(const QString &msg);
+
+private:
+    Ui::CannyParameterCalculatorClass ui;
+
+    // the source image
+    cv::Mat							mOriginalImage;
+    cv::Mat							mTransformedImage;
+    double							mThreshold1;
+    double							mThreshold2;
+    bool							mL2Gradient;
+
+
+};
+
+#endif // CANNYPARAMETERCALCULATOR_H
---
+++ trunk/src/GUI/IPSModule/ImageProcessingSystem/WaveHeightIPS/RealTimeDataDisplayer/realtimedatadisplayer.h
@@ -0,0 +1,70 @@
+#ifndef REALTIMEDATADISPLAYER_H
+#define REALTIMEDATADISPLAYER_H
+
+#include <QtGui/QWidget>
+#include <qgraphicsscene.h>
+#include <qpainter.h>
+#include <qdialog.h>
+
+#include <vector>
+
+#include "ui_realtimedatadisplayer.h"
+
+
+class RealTimeDataDisplayer : public QDialog
+{
+    Q_OBJECT
+
+public:
+    RealTimeDataDisplayer(QWidget *parent = 0);
+    ~RealTimeDataDisplayer();
+
+    /* Sets the maximum possible value of the X axis in seconds*/
+    void setMaxXValue(float max);
+
+    /* Sets the maximum possible value of the Y axis in cm */
+    void setMaxYValue(float max);
+
+    /* Set scale (pixel/cm relation), so, how much value a pixel in cm */
+    void setMeasureScale(float s);
+
+    /* Sets the Time scale (pixel/seconds) */
+    void setTimeScale(float s);
+
+    /* Builds the axis and init the GUI
+     * RETURNS:
+     * 	true		on success
+     *	false		on error
+     */
+    bool init(void);
+
+    /* Adds a new point to draw.
+     * Params:
+     * 	@time		the time in seconds
+     * 	@height		the height in cm
+     */
+    void addNewPoint(float time, float height);
+
+private:
+    /* Creates the both axis */
+    void createAxis(void);
+
+    /* Creates a point */
+    void createPoint(float x, float y);
+
+private:
+    Ui::RealTimeDataDisplayerClass ui;
+    QGraphicsScene					mScene;
+
+    // Axis max values
+    float							mXMaxValue;
+    float							mYMaxValue;
+    // Scale in cm
+    float							mMeasureScale;
+    float							mTimeScale;
+    // the points
+    std::vector<QGraphicsEllipseItem *> mPoints;
+
+};
+
+#endif // REALTIMEDATADISPLAYER_H
