3860a57c2745566cca7962d9e396efd79a8a39ac
aguperezpala@gmail.com <aguperezpala@gmail.com@06da5462-caec-b45d-aab9-b80d20edd223>
2012-01-03 04:56:03 +0000

    Before completely new design.

--- trunk/src/Tests/v4l2/GetInputInformation/main.cpp
+++ trunk/src/Tests/v4l2/GetInputInformation/main.cpp
@@ -14,6 +14,8 @@
 
 #include <linux/videodev2.h>
 
+static int fd;
+
 static void printFrameSizeEnum(struct v4l2_frmsizeenum *argp)
 {
 
@@ -47,9 +49,8 @@ static void printFrameSizeEnum(struct v4l2_frmsizeenum *argp)
 		}
 }
 
-static void getResolutions(int fd)
+static void getResolutions(struct v4l2_frmsizeenum &argp)
 {
-	struct v4l2_frmsizeenum argp;
 	argp.index = 0;
 
 	int err = ioctl(fd, VIDIOC_ENUM_FRAMESIZES, &argp);
@@ -91,16 +92,120 @@ static void getResolutions(int fd)
 }
 
 
+void getCap(struct v4l2_format &chan)
+{
+
+	if (ioctl(fd, VIDIOC_G_FMT, &chan) == -1){
+		perror("err");
+		return;
+	}
+	printf("Device format: %d\n", chan.type);
+
+	switch(chan.type){
+	case V4L2_BUF_TYPE_VIDEO_CAPTURE:
+		// is pix
+		std::cout << "Width: " <<  chan.fmt.pix.width << std::endl <<
+		"Height: " << chan.fmt.pix.height << std::endl;
+		break;
+
+	default:
+		std::cout << "Error: unknown type???\n";
+		return;
+	}
+}
+struct v4l2_queryctrl queryctrl;
+	struct v4l2_querymenu querymenu;
+
+static void
+	enumerate_menu (void)
+	{
+	        printf ("  Menu items:\n");
+
+	        memset (&querymenu, 0, sizeof (querymenu));
+	        querymenu.id = queryctrl.id;
+
+	        for (querymenu.index = queryctrl.minimum;
+	             querymenu.index <= queryctrl.maximum;
+	              querymenu.index++) {
+	                if (0 == ioctl (fd, VIDIOC_QUERYMENU, &querymenu)) {
+	                        printf ("  %s\n", querymenu.name);
+	                } else {
+	                        perror ("VIDIOC_QUERYMENU");
+	                        return;
+	                }
+	        }
+	}
+
 int main(int argc, char **args)
 {
-	int fd = open("/dev/video0", O_RDWR);
+	fd = open("/dev/video0", O_RDWR);
 	if(fd == -1){
 		perror("Error opening /dev/video0");
 		return -1;
 	}
 
+
+
+//
+//	memset (&queryctrl, 0, sizeof (queryctrl));
+//
+//	for (queryctrl.id = V4L2_CID_BASE;
+//	     queryctrl.id < V4L2_CID_LASTP1;
+//	     queryctrl.id++) {
+//	        if (0 == ioctl (fd, VIDIOC_QUERYCTRL, &queryctrl)) {
+//	                if (queryctrl.flags & V4L2_CTRL_FLAG_DISABLED)
+//	                        continue;
+//
+//	                printf ("Control %s\n", queryctrl.name);
+//
+//	                if (queryctrl.type == V4L2_CTRL_TYPE_MENU)
+//	                        enumerate_menu ();
+//	        } else {
+//	                if (errno == EINVAL)
+//	                        continue;
+//
+//	                perror ("VIDIOC_QUERYCTRL");
+//	                return -1;
+//	        }
+//	}
+//
+//	for (queryctrl.id = V4L2_CID_PRIVATE_BASE;;
+//	     queryctrl.id++) {
+//	        if (0 == ioctl (fd, VIDIOC_QUERYCTRL, &queryctrl)) {
+//	                if (queryctrl.flags & V4L2_CTRL_FLAG_DISABLED)
+//	                        continue;
+//
+//	                printf ("Control %s\n", queryctrl.name);
+//
+//	                if (queryctrl.type == V4L2_CTRL_TYPE_MENU)
+//	                        enumerate_menu ();
+//	        } else {
+//	                if (errno == EINVAL)
+//	                        break;
+//
+//	                perror ("VIDIOC_QUERYCTRL");
+//	                return -1;
+//	        }
+//	}
+
+
+//
+//	struct v4l2_format chan;
+//	getCap(chan);
+//	return 0;
+
+	struct v4l2_frmivalenum argp2;
+	int err = ioctl(fd, VIDIOC_ENUM_FRAMEINTERVALS, &argp2);
+	if(err == -1){
+		perror("Error VIDIOC_ENUM_FRAMEINTERVALS: ");
+	}
+
+	return 0;
+
 	struct v4l2_input input;
 	int index;
+
+
 
 	if (-1 == ioctl (fd, VIDIOC_G_INPUT, &index)) {
 	        perror ("VIDIOC_G_INPUT");
@@ -117,7 +222,9 @@ int main(int argc, char **args)
 
 	printf ("Current input: %s\n", input.name);
 
-	getResolutions(fd);
+
+	struct v4l2_frmsizeenum argp;
+	getResolutions(argp);
 
 	return 0;
 }
