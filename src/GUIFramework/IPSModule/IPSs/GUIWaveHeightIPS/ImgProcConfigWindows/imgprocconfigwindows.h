b59f61a911e4545f209c0af9db3fd4d2564f5612
aguperezpala@gmail.com <aguperezpala@gmail.com@06da5462-caec-b45d-aab9-b80d20edd223>
2012-01-10 18:05:59 +0000

    Working on GUICannyBorderDetector.

--- src/GUIFramework/IPSModule/IPSs/GUIWaveHeightIPS/ImgProcConfigWindows/imgprocconfigwindows.h
+++ src/GUIFramework/IPSModule/IPSs/GUIWaveHeightIPS/ImgProcConfigWindows/imgprocconfigwindows.h
@@ -28,6 +28,52 @@ public:
     	mImgGenerator = ig;
     }
 
+	/* Function used to load the configurations from a xml file
+	 * Returns:
+	 * 	errCode
+	 */
+	virtual errCode loadConfig(const TiXmlElement *)
+	{
+		debug("Unimplemented\n");
+		return FEATURE_NOT_SUPPORTED;
+	}
+
+	/* Save the data to an xml and return it.
+	 * Returns:
+	 * 	0			on Error
+	 * 	xml			on success */
+	virtual std::auto_ptr<TiXmlElement> getConfig(void) const
+	{
+		debug("unimplemented\n");
+		return std::auto_ptr<TiXmlElement>(0);
+	}
+
+	/* Function used to retrieve the info about the ConfigWindow. This is
+	 * an optional function.
+	 */
+	virtual QString getInfo(void) const {return QString("");}
+
+	/* Function called when the window get visible */
+	virtual void windowVisible(void) {};
+
+	/* Function called when the window get invisible */
+	virtual void windowInvisible(void) {};
+
+	/* Function called when the user click on "Next Button" and the ConfigWindow
+	 * will be hide and show the next one. This function shall return NO_ERROR
+	 * if the ConfigWindow can be closed, or the error and the string error
+	 * by param
+	 */
+	virtual errCode finish(QString &error) = 0;
+
+	/* We have to implement the signal used to advise when the actual ConfigWindow
+	 * is finish automatically.
+	 */
+signals:
+	// Advise when the configurator dialog is finish sending the errCode
+	// associated and the info about the error associated.
+	void doneSignal(int, QString info);
+
 protected:
     ImageGenerator		*mImgGenerator;
 };
