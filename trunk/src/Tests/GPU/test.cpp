ace7693f85e2bda42676140c3083bb6375c00a7f
aguperezpala@gmail.com <aguperezpala@gmail.com@06da5462-caec-b45d-aab9-b80d20edd223>
2012-04-26 20:13:51 +0000

    adding GPU Test

---
+++ trunk/src/Tests/GPU/test.cpp
@@ -0,0 +1,87 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 02/09/2011
+ *      Author: agustin
+ *
+ *
+ * OpenVC License
+ *
+ * Copyright (C) 2000-2008, Intel Corporation, all rights reserved.
+ * Copyright (C) 2009, Willow Garage Inc., all rights reserved.
+ * Third party copyrights are property of their respective owners.
+ *
+ * This software is provided by the copyright holders and contributors "as is" and
+ * any express or implied warranties, including, but not limited to, the implied
+ * warranties of merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the Intel Corporation or contributors be liable for any direct,
+ * indirect, incidental, special, exemplary, or consequential damages
+ * (including, but not limited to, procurement of substitute goods or services;
+ * loss of use, data, or profits; or business interruption) however caused
+ * and on any theory of liability, whether in contract, strict liability,
+ * or tort (including negligence or otherwise) arising in any way out of
+ * the use of this software, even if advised of the possibility of such damage.
+ */
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/gpu/gpu.hpp>
+#include <opencv2/gpu/gpumat.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+
+int main(int argc, char **args)
+{
+	if(argc < 2){
+		std::cout << "Error, falta nombre archivo\n";
+		return -1;
+	}
+
+	cv::Mat img = cv::imread(args[1]);
+
+	if(img.empty()){
+		std::cout << "Error: Img " << args[1] << "not exists\n";
+		return -1;
+	}
+
+	double th1 = 15, th2 = 30;
+	// check canny param
+	if(argc == 4){
+		std::stringstream s1;
+		s1 << args[2];
+		s1 >> th1;
+		std::stringstream s2;
+		s2 << args[3];
+		s2 >> th2;
+
+		std::cout << "Usando parametros: " << th1 << "\t" << th2 << std::endl;
+	}
+
+	cv::namedWindow("Real Image");
+	cv::namedWindow("Canny Image");
+
+	// port to gpu
+	cv::gpu::GpuMat gm;
+
+	gm.upload(img);
+//	cv::gpu::cvtColor(gm, gm2, CV_BGR2GRAY);
+//	cv::gpu::Canny(gm, gm, th1, th2);
+
+	// download again to the memory
+//	gm.download(img);
+	cv::imshow("Real Image", img);
+
+
+//	cv::cvtColor(img, img, CV_BGR2GRAY);
+//	cv::Canny(img, img, th1, th2);
+//
+//	cv::imshow("Canny Image", img);
+	cv::waitKey();
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
